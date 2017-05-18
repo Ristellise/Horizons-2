015036fdacd42ef58a67de1ec3f635347617972a
Joshwoo70 <joshwoo71@gmail.com>
2017-05-18 17:09:02 +0800

    Update Splashes [CI Skip]

--- src/Splashes.py
+++ src/Splashes.py
@@ -2,5 +2,5 @@ from src import ConfigurationHandler
 
 splashes = []
 
-splashes.append('"I cannot Taste Anything"- Velvet Crowe')
+splashes.append(""""Can't taste a thing"  - Velvet Crowe""")
 splashes.append('"Playing Minecraft on the switch is a strange and fun experience for me!" - Notch')
