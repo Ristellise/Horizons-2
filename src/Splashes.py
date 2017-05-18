9150e1b5586498ad3c79bb2cbd366cad65a073a8
Joshwoo70 <joshwoo71@gmail.com>
2017-05-18 18:27:25 +0800

    Update Splashes [CI Skip]

--- src/Splashes.py
+++ src/Splashes.py
@@ -4,3 +4,4 @@ splashes = []
 
 splashes.append(""""Can't taste a thing"  - Velvet Crowe""")
 splashes.append('"Playing Minecraft on the switch is a strange and fun experience for me!" - Notch')
+splashes.append()
