4f7f1d8efd4cf1ccf9588ed6caa9bc2ee0ea5e4a
Joshwoo70 <joshwoo71@gmail.com>
2017-05-21 14:25:21 +0800

    Continue Work on universe Wrapper and Mod loader [CI Skip]

--- src/ModLoader.py
+++ src/ModLoader.py
@@ -1,5 +1,14 @@
-import re
+import openpyxl
 import os
-def loadmod(absLocation):
-    for root, dirs, files in os.walk(absLocation):
-        if files == re.search('\.xlsx',files)
+
+
+def loadmod(abslocation):
+    for root, dirs, files in os.walk(abslocation):
+        for name in files:
+            if name == "Resources.xlsx":
+                wb = openpyxl.load_workbook(filename=name)
+                # Set active to first page... (Resources)
+                wb.active = 0
+                worksheet = wb.active
+                row1 = list(worksheet.rows[0])
+                print(row1)
--- src/UniverseGen/UniverseWrapper.py
+++ src/UniverseGen/UniverseWrapper.py
@@ -1,4 +1,8 @@
-def generateuniverse(name,*args):
+import random
+from src.UniverseGen import Elliptical
+
+
+def generateuniverse(name, *args):
     """
     :param name: Name of Universe
     :param args: type: Type of generation
@@ -6,13 +10,25 @@ def generateuniverse(name,*args):
                  clusters : Number of Clusters
                  starpercluster : Stars Per clusters
                  distrostars : Distribution of Star Number in each Cluster
+                 Galaxylength : How wide the galaxy is
+                 PNGSIZE = Well duh?
+                 seed = seed in args or random seed...
     :return: 
     """
-    for(type, NoStars, clusters, starpercluster, distrostars) in args:
+    for (type, NoStars, clusters, starpercluster, distrostars, Galaxylength, PNGSIZE, seed) in args:
         TYPE = type
         if clusters is None or 0:
-            NUMC = 1
+            Elliptical.NUMC = 1
         else:
-            NUMC = clusters
-        NUMSTR = NoStars
-        NUMCLUS = clusters
+            Elliptical.NUMC = clusters
+        Elliptical.NUMSTR = NoStars
+        Elliptical.NUMCLUS = clusters
+        Elliptical.GALX = Galaxylength
+        Elliptical.GALY = Galaxylength
+        Elliptical.CLUSRADA = Elliptical.GALX // 12
+        Elliptical.DISCLRADA = Elliptical.CLUSRADA // 5
+        RAND = random.seed(seed)
+    if type == "0":
+        Elliptical.generateclusters()
+        Elliptical.generateStars()
+        Elliptical.drawtopng("ellipticalgalaxy" + str(RAND) + "-" + str(name) + ".png")
