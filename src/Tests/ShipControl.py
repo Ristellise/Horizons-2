fda9846c28905b03e5553b6865220d9f917106a3
Joshwoo70 <joshwoo71@gmail.com>
2017-05-24 16:13:48 +0800

    Update EventHandler [CI Skip]

--- src/Tests/ShipControl.py
+++ src/Tests/ShipControl.py
@@ -5,7 +5,9 @@ pygame.init()
 Display = pygame.display.set_mode((1600,900))
 Shipimage = pygame.image.load_extended('Crusier.png')
 ClockFPS = pygame.time.Clock()
+
 while True:
     Display.fill((0, 0, 0))
     eventhandler.eventhandle()
     ClockFPS.tick(60)
+
--- src/Tests/eventhandler.py
+++ src/Tests/eventhandler.py
@@ -1,15 +1,22 @@
 import pygame
 import logging
 
+logging.basicConfig(level=logging.DEBUG)
+
 
 def eventhandle():
     pygame.event.pump()
     event = pygame.event.poll()
-    if event.type == 0:  # Act. LIKE. A. FILTER
-        pass  # Coo. Coo.
+    if event.type == 0:  # Act. LIKE. A. FILTER.
+        pass  # Coo... Coo... *Blushing Intensifies*
     else:
-        logging.DEBUG(event.dict)
+        logging.debug(event.__dict__)
+        logging.debug(event)
 
 
-if __name__ == "__Main__":
-    logging.basicConfig(level=logging.DEBUG)
+def isPressed(key, *args):
+    """
+    :param key: key(in numbers... not the unicode one!)
+    :param args: can be(True, False, None)
+    :return: If key == None, return True or False Depending on key being pressed.
+    """
