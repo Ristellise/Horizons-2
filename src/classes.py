aecdb019d9e450b01122ca7e9dba07f543e5e2ec
Joshwoo70 <joshwoo71@gmail.com>
2017-06-25 20:58:31 +0800

    Most* of the classes doesnt work https://i.ytimg.com/vi/8j3ITU0Osws/hqdefault.jpg

--- src/classes.py
+++ src/classes.py
@@ -7,6 +7,11 @@ from src import Gconstants, Utils
 
 
 class Generic:
+    """
+    Generic BaseClass
+    Does nothing on it's own
+    Extend it!
+    """
     def __init__(self, x, y):
         self.x = x
         self.y = y
@@ -14,7 +19,8 @@ class Generic:
 
 class image(Generic):
     """
-    image rendering
+    Image Base
+    Extended from Generic
     """
 
     def __init__(self, x, y, selfimage):
@@ -26,9 +32,9 @@ class image(Generic):
         """
         super().__init__(x, y)
         self.image = pygame.image.load(selfimage)
-        self.size = image.get_rect().size
+        self.size = image.get_size()
         print(self.size)
-        self.rect = pygame.Rect(self.x, self.y, self.h, self.w)
+        self.rect = image.get_size()
 
     def draw(self, canvas):
         canvas.blit(self.image, (self.x, self.y))
@@ -38,18 +44,25 @@ class image(Generic):
 
 
 class text(Generic):
-    def __init__(self, text, x, y, color, AA):
+    def __init__(self, selftext, x, y, color, aa):
         super().__init__(x, y)
         self.color = color
-        self.antialias = AA
-        self.text = text
+        self.antialias = aa
+        self.text = selftext
 
     def draw(self, canvas):
         print(self.color, self.antialias, self.text)
         if Gconstants.Font is None:
             print('font is none!')
-        render = Gconstants.Font.render(self.text, self.antialias, pygame.Color(self.color),pygame.Color(255, 255, 255))
+        render = Gconstants.Font.render(self.text, self.antialias, pygame.Color(self.color),
+                                        pygame.Color(255, 255, 255))
         canvas.blit(render, (self.x, self.y))
+
+
+# Ship class Extends from image class
+class shipclass(image):
+    def __init__(self, x, y, selfimage):
+        super().__init__(x, y, selfimage)
 
 
 def setupfont():
